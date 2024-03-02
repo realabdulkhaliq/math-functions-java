98c93a16d7351602019e5c82bcce295f96983e43
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-02 11:00:32 +0500

    Triangle 3

---
+++ Math_Functions/src/triagle3.java
@@ -0,0 +1,25 @@
+
+public class triagle3 {
+    public static void main(String[] args){
+
+    for (int i=1; i<=10; i++)
+		{
+			System.out.println();
+			for (int j=i; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+
+                        for (int k=1; k<=i; k++)
+                        {
+		 	System.out.print(k);
+                        }
+	        }
+	System.out.println();
+
+
+
+
+
+    }
+}
