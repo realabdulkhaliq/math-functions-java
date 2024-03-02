d82ca430f893c6679cac67e4c8269052e27cba0e
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-02 10:59:48 +0500

    Triangle

---
+++ Math_Functions/src/triangle1.java
@@ -0,0 +1,23 @@
+
+public class triangle1 {
+    public static void main(String[] args){
+
+        for (int i=1; i<=10; i++)
+{
+System.out.println();
+for (int j=1; j<=i; j++)
+{
+System.out.print("*");
+}
+}
+System.out.println();
+        
+
+
+
+
+
+                          
+    }
+
+}
