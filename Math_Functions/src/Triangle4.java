9486aaa5c795155239b620b99e111b513f344a41
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-02 11:00:50 +0500

    Triangle 4

---
+++ Math_Functions/src/Triangle4.java
@@ -0,0 +1,21 @@
+
+public class Triangle4 {
+     public static void main(String[] args){
+
+    for (int i=10; i>=1; i--)
+		{
+			System.out.println();
+			for (int j=9; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+
+                        for (int k=1; k<=i; k++)
+                        {
+		 	System.out.print("*");
+                        }
+	        }
+	System.out.println();
+    }
+
+}
