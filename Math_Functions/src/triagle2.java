455f5c72b53a6f85a4630e841932a81c242253a8
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-02 11:00:13 +0500

    Triangle

---
+++ Math_Functions/src/triagle2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class triagle2 {
+    public static void main(String[] args){
+   for  (int i=10; i>=1; i--)
+{
+System.out.println();
+for (int j=1; j<=i; j++)
+{
+System.out.print(j);
+}
+}
+System.out.println();
+
+}
+}
