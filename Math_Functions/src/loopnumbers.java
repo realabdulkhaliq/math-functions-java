265a224a0681f31ff909f3cbc8930fc731b4eb43
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-03 15:08:17 +0500

    Loop numbers

---
+++ Math_Functions/src/loopnumbers.java
@@ -0,0 +1,19 @@
+
+public class loopnumbers {
+         public static void main(String Args[]){
+
+             for(int i=1;i<=9;i++)
+{
+if(i<5)
+{
+
+System.out.print(i);
+}
+else
+{
+System.out.print(10-i);
+}
+}
+System.out.println();
+}
+}
